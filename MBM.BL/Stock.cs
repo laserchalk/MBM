587242ef8ed0ed500ad6fbed9251180769b98bf8
laserchalk <33247144+laserchalk@users.noreply.github.com>
2017-11-06 21:08:14 +1100

    Altered Stock class

--- MBM.BL/Stock.cs
+++ MBM.BL/Stock.cs
@@ -10,29 +10,122 @@ namespace MBM.BL
     {
         public Stock()
         {
-            Prices = new Price[5];
+
         }
 
-        public Stock(string exchange, string symbol, uint volume, DateTime date)
+        public Stock(string exchange, string symbol, uint volume, DateTime date, decimal priceHigh, decimal priceLow, decimal priceOpen, decimal priceClose, decimal priceCloseAdjusted)
         {
             this.Exchange = exchange;
             this.Symbol = symbol;
             this.Volume = volume;
             this.Date = date;
+            this.PriceHigh = priceHigh;
+            this.PriceLow = priceLow;
+            this.PriceOpen = priceOpen;
+            this.PriceClose = priceClose;
+            this.PriceCloseAdjusted = priceCloseAdjusted;
         }
 
-        public Stock(float priceOpen, float priceClose, float priceCloseAdjust, float priceHigh, float priceLow)
+        public uint ID { get; set; }
+
+        private string _exchange;
+        public string Exchange {
+            get
+            {
+                return _exchange;
+            }
+            set
+            {
+                if(value.Length > 4) throw new ArgumentException("Exchange must have less than 5 characters");
+                _exchange = value;
+            }
+        }
+
+        private string _symbol;
+        public string Symbol
         {
-            
+            get
+            {
+                return _symbol;
+            }
+            set
+            {
+                if (value.Length > 3) throw new ArgumentException("Symbol must have less than 4 characters");
+                _symbol = value;
+            }
         }
-
-        public string Exchange { get; set; }
-        public string Symbol { get; set; }
         public uint Volume { get; set; }
         public DateTime Date { get; set; }
-        public Price[] Prices { get; set; }
 
-        //test 3
+        private decimal _priceHigh;
+        public decimal PriceHigh
+        {
+            get
+            {
+                return _priceHigh;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException("PriceHigh cannot be negative");
+                _priceHigh = value;
+            }
+        }
+
+        private decimal _priceLow;
+        public decimal PriceLow
+        {
+            get
+            {
+                return _priceLow;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException("PriceLow cannot be negative");
+                _priceLow = value;
+            }
+        }
+
+        private decimal _priceOpen;
+        public decimal PriceOpen
+        {
+            get
+            {
+                return _priceOpen;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException("PriceOpen cannot be negative");
+                _priceOpen = value;
+            }
+        }
+
+        private decimal _priceClose;
+        public decimal PriceClose
+        {
+            get
+            {
+                return _priceClose;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException("PriceClose cannot be negative");
+                _priceClose = value;
+            }
+        }
+
+        private decimal _priceCloseAdjusted;
+        public decimal PriceCloseAdjusted
+        {
+            get
+            {
+                return _priceCloseAdjusted;
+            }
+            set
+            {
+                if (value < 0) throw new ArgumentException("PriceCloseAdjusted cannot be negative");
+                _priceCloseAdjusted = value;
+            }
+        }
 
 
     }
--- MBM.BLTest/StockTest.cs
+++ MBM.BLTest/StockTest.cs
@@ -7,6 +7,144 @@ namespace MBM.BLTest
     [TestClass]
     public class StockTest
     {
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PriceHighOutOfRangeTest()
+        {
+            //-- Arrange
+            Stock stock = new Stock();
+            decimal input = -1.00m;
 
+            //-- Act
+            try
+            {
+                stock.PriceHigh = input;
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("PriceHigh cannot be negative", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PriceLowOutOfRangeTest()
+        {
+            //-- Arrange
+            Stock stock = new Stock();
+            decimal input = -1.00m;
+
+            //-- Act
+            try
+            {
+                stock.PriceLow = input;
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("PriceLow cannot be negative", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PriceOpenOutOfRangeTest()
+        {
+            //-- Arrange
+            Stock stock = new Stock();
+            decimal input = -1.00m;
+
+            //-- Act
+            try
+            {
+                stock.PriceOpen = input;
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("PriceOpen cannot be negative", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PriceCloseOutOfRangeTest()
+        {
+            //-- Arrange
+            Stock stock = new Stock();
+            decimal input = -1.00m;
+
+            //-- Act
+            try
+            {
+                stock.PriceClose = input;
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("PriceClose cannot be negative", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void PriceCloseAdjustedOutOfRangeTest()
+        {
+            //-- Arrange
+            Stock stock = new Stock();
+            decimal input = -1.00m;
+
+            //-- Act
+            try
+            {
+                stock.PriceCloseAdjusted = input;
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("PriceCloseAdjusted cannot be negative", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void ExchangeTooLongTest()
+        {
+            //-- Arrange
+            Stock stock = new Stock();
+            string input = "ABCDE";
+
+            //-- Act
+            try
+            {
+                stock.Exchange = input;
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Exchange must have less than 5 characters", ex.Message);
+                throw;
+            }
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void SymbolTooLongTest()
+        {
+            //-- Arrange
+            Stock stock = new Stock();
+            string input = "ABCD";
+
+            //-- Act
+            try
+            {
+                stock.Symbol = input;
+            }
+            catch (Exception ex)
+            {
+                Assert.AreEqual("Symbol must have less than 4 characters", ex.Message);
+                throw;
+            }
+        }
     }
 }
